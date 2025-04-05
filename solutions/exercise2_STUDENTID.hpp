680994b1b56c01f935df77f78ea4cfe2c3be6c0f
jemin hwangbo <jemin.hwangbo@gmail.com>
2025-04-05 17:03:11 +0900

    exercise 2

---
+++ solutions/exercise2_STUDENTID.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+/// do not change the name of the method
+inline Eigen::Vector3d getLinearVelocity (const Eigen::VectorXd& gc, const Eigen::VectorXd& gv) {
+    //////////////////////////
+    ///// Your Code Here /////
+    //////////////////////////
+
+    return Eigen::Vector3d::Ones(); /// replace this
+}
+
+/// do not change the name of the method
+inline Eigen::Vector3d getAngularVelocity (const Eigen::VectorXd& gc, const Eigen::VectorXd& gv) {
+  //////////////////////////
+  ///// Your Code Here /////
+  //////////////////////////
+
+  return Eigen::Vector3d::Ones(); /// replace this
+}
